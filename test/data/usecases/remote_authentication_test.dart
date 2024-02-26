69d2bc1a0ff8c367dee87d5c9715315374500f5a
Gabriel Moreira <g.tabordamoreira@gmail.com>
2024-02-26 17:55:59 -0400

    move SUT creation to a setup method

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,6 +1,9 @@
 import 'package:faker/faker.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import 'remote_authentication_test.mocks.dart';
 
 class RemoteAuthentication {
   final HttpClientProject httpClient;
@@ -8,24 +11,46 @@ class RemoteAuthentication {
 
   RemoteAuthentication({required this.httpClient, required this.url});
 
-  Future<void> auth() async {}
+  Future<void> auth() async {
+    await httpClient.request(
+      url: url,
+      method: 'post',
+    );
+  }
 }
 
 abstract class HttpClientProject {
-  Future<void> request({required String url});
+  Future<void> request({
+    required String url,
+    required String method,
+  });
 }
 
-class HttpClientSpy extends Mock implements HttpClientProject {}
-
+// Generate a MockClient using the Mockito package.
+// Create new instances of this class in each test.
+@GenerateMocks([HttpClientProject])
 void main() {
-  test('Should call HttpClient with corrent URL', () async {
-    final httpClient = HttpClientSpy();
-    final url = faker.internet.httpUrl();
+  late RemoteAuthentication sut;
+  late MockHttpClientProject httpClientMock;
+  late String url;
 
-    final sut = RemoteAuthentication(httpClient: httpClient, url: url);
+  setUp(() {
+    httpClientMock = MockHttpClientProject();
+    url = faker.internet.httpUrl();
+    sut = RemoteAuthentication(httpClient: httpClientMock, url: url);
+  });
+
+  test('Should call HttpClient with corrent values', () async {
+    when(httpClientMock.request(
+      url: url,
+      method: 'post',
+    )).thenAnswer((_) => Future.value());
 
     await sut.auth();
 
-    verify(httpClient.request(url: url));
+    verify(httpClientMock.request(
+      url: url,
+      method: 'post',
+    ));
   });
 }
