192fd63bb97e713c10a220aa701c6863147448ff
Gabriel Moreira <g.tabordamoreira@gmail.com>
2024-02-27 15:16:00 -0400

    ensure RemoteAuthentication throws UnexpectedError on 500

--- test/data/models/remote_authentication_test.dart
+++ test/data/models/remote_authentication_test.dart
@@ -74,4 +74,16 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if HttpClient returns 500', () async {
+    when(httpClientMock.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body'),
+    )).thenThrow(HttpError.serverError);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
