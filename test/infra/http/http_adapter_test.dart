1e4378da32249c9ad938674794b00e39153d6fd3
Gabriel Moreira <g.tabordamoreira@gmail.com>
2024-02-27 21:22:13 -0400

    ensure HttpAdapter returns null if post returns 204 with data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -31,7 +31,11 @@ class HttpAdapter implements HttpClientProject {
       headers: headers,
       body: body != null ? jsonEncode(body) : null,
     );
-    return response.body.isEmpty ? null : jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    }
+    return null;
   }
 }
 
@@ -107,6 +111,14 @@ void main() {
 
     test('Should return null if post returns 204', () async {
       mockResponse(204, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+
+    test('Should return null if post returns 204 with data', () async {
+      mockResponse(204);
 
       final response = await sut.request(url: url, method: 'post');
 
